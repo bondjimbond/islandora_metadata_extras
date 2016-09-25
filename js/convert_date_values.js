12f7b0d4b16d8ab578f8af187ab6ba8e3e413089
Mark Jordan <mjordan@sfu.ca>
2016-09-25 20:12:47 +0000

    Addded support for replacing yyyy-mm dates to #3.

--- js/convert_date_values.js
+++ js/convert_date_values.js
@@ -1,10 +1,11 @@
 (function ($) {
   $(document).ready(function() {
     $("dl.islandora-metadata-fields > dd").each(function() {
-      // If a field's value matches dddd-dd-dd, convert it to its
-      // textual equivalent. Does not know about locales.
       var fieldValue = $(this).text();
       fieldValue = $.trim(fieldValue);
+
+      // If a field's value matches dddd-dd-dd, convert it to its
+      // textual equivalent, respecting locale-specific month names.
       var datePattern = /^\d\d\d\d\-\d\d\-\d\d$/;
       if (datePattern.test(fieldValue)) {
         dateParts = fieldValue.split("-");
@@ -17,6 +18,20 @@
         var textDate = month + ' ' + day + ', ' + year;
         $(this).text(textDate);
       }
+
+      // If a field's value matches dddd-dd, convert it to its
+      // textual equivalent, respecting locale-specific month names.
+      datePattern = /^\d\d\d\d\-\d\d$/;
+      if (datePattern.test(fieldValue)) {
+        dateParts = fieldValue.split("-");
+        var year = dateParts[0];
+        var month = dateParts[1];
+        var date = new Date(year, month);
+        month = date.toLocaleString(Drupal.settings.islandora_metadata_extras.locale, { month: "long" });
+        var textDate = month + ' ' + year;
+        $(this).text(textDate);
+      }
+
     }
  )});
 })(jQuery);
