0d4c3696872b46b309e464a195ad7846301172cd
Mark Jordan <mjordan@sfu.ca>
2016-09-26 03:44:11 +0000

    Address #5.

---
+++ js/hide_empty_values_in_search_results.js
@@ -0,0 +1,13 @@
+(function ($) {
+  $(document).ready(function() {
+    $("dl.solr-fields islandora-inline-metadata > dd").each(function() {
+      // Value of the <dd> cannot be emtpy or contain only puncutation.
+      if ($.trim($(this).text()) == '' || $.trim($(this).text()).match(/^[.,;:\-]$/)) {
+        // Hide the <dd>.
+        $(this).hide();
+        // And hide the corresponding <dt>.
+        $(this).prev().hide();
+      }
+    }
+ )});
+})(jQuery);
