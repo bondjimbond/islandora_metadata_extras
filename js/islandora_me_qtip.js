2d20b1deab47df9d6ac8aa509617929950e39e2f
Mark Jordan <mjordan@sfu.ca>
2016-09-29 15:09:27 +0000

    Added js file that should have been included in last commit.

---
+++ js/islandora_me_qtip.js
@@ -0,0 +1,15 @@
+(function ($) {
+  $(document).ready(function() {
+    $("dl.solr-thumb > dt > a").each(function() {
+      $(this).after(Drupal.settings.islandora_metadata_extras.tooltip_markup);
+      $(this).qtip({
+        content: {
+          text: $(this).next('.tooltiptext')
+        }
+      });
+    }
+ )});
+})(jQuery);
+
+
+
