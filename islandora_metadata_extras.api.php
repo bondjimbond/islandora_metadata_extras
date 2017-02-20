375d4324f363b4ac42825ed279426a1a566d2218
Mark Jordan <mjordan@sfu.ca>
2017-02-20 16:48:02 +0000

    Updated API example.

--- islandora_metadata_extras.api.php
+++ islandora_metadata_extras.api.php
@@ -6,7 +6,7 @@
  */
 
 /**
- * Allows other modules to perform a drupal_alter() on Dublin Core values.
+ * Allows modules to alter Dublin Core values that get inserted into HTML meta tags.
  *
  * @param object $dc
  *   An array of Dublin Core elements, each member of which is itself
@@ -16,5 +16,14 @@
  *  The current Islandora object.
  */
 function mymodule_islandora_metadata_extras_dc_in_head_alter(&$dc, $object) {
-  $dc['title'][0] = "Out with the old title, in with the new!";
+  // Override the title that gets inserted into the HTML HEAD meta tags for
+  // a specific object.
+  if ($object->id == 'islandora:1234' && isset($dc['title'][0])) {
+    $dc['title'][0] = "Out with the old title, in with the new!";
+  }
+
+  // Show at most 10 DC subject elements.
+  if (isset($dc['subject']) && count($dc['subject']) > 10) {
+    $dc['subject'] = array_slice($dc['subject'], 0, 10);
+  }
 }
