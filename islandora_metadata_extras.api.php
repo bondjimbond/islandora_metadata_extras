ed11339a2890612c99de8daca29a2858017376ad
Mark Jordan <mjordan@sfu.ca>
2017-02-20 16:11:08 +0000

    Address #11.

---
+++ islandora_metadata_extras.api.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file
+ * Documents the hooks this module defines.
+ */
+
+/**
+ * Allows other modules to perform a drupal_alter() on Dublin Core values.
+ *
+ * @param object $dc
+ *   An array of Dublin Core elements, each member of which is itself
+ *   an array.
+ *
+ * @param object $object
+ *  The current Islandora object.
+ */
+function mymodule_islandora_metadata_extras_alter(&$dc, $object) {
+  $dc['title'][0] = "Out with the old title, in with the new!";
+}
