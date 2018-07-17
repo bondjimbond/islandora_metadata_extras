e9e4ea3b2451d2186574560ea0bc8241c06de22b
Mark Jordan <mjordan@sfu.ca>
2018-07-17 14:48:17 +0000

    Work on #21.

---
+++ theme/islandora-solr-metadata-display.tpl.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @file
+ * Islandora_solr_metadata display template.
+ *
+ * Variables available:
+ * - $solr_fields: Array of results returned from Solr for the current object
+ *   based upon defined display configuration(s). The array structure is:
+ *   - display_label: The defined display label corresponding to the Solr field
+ *     as defined in the configuration in translatable string form.
+ *   - value: An array containing all the result(s) found for the specific field
+ *     in Solr for the current object when queried against Solr.
+ * - $found: Boolean indicating if a Solr doc was found for the current object.
+ * - $not_found_message: A string to print if there was no document found in
+ *   Solr.
+ *
+ * @see template_preprocess_islandora_solr_metadata_display()
+ * @see template_process_islandora_solr_metadata_display()
+ */
+?>
+<?php if ($found):
+  if (!(empty($solr_fields) && variable_get('islandora_solr_metadata_omit_empty_values', FALSE))):?>
+<fieldset <?php $print ? print('class="islandora islandora-metadata"') : print('class="islandora islandora-metadata collapsible collapsed"');?>>
+  <legend><span class="fieldset-legend"><?php print t('Details'); ?></span></legend>
+  <div class="fieldset-wrapper">
+
+  <div>Templated metadata goes here.</div>
+  <div>
+    <pre>
+      <!-- To do: In process function, replace $solr_fields with the output of the template. -->
+      <?php var_export($solr_fields); ?>
+    </pre>
+  </div>
+
+  </div>
+</fieldset>
+<?php endif; ?>
+<?php else: ?>
+  <fieldset <?php $print ? print('class="islandora islandora-metadata"') : print('class="islandora islandora-metadata collapsible collapsed"');?>>
+    <legend><span class="fieldset-legend"><?php print t('Details'); ?></span></legend>
+    <?php //XXX: Hack in markup for message. ?>
+    <div class="messages--warning messages warning">
+      <?php print $not_found_message; ?>
+    </div>
+  </fieldset>
+<?php endif; ?>
